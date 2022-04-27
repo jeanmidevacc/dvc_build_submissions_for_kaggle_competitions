174927093ce728f498ade80356f4e54993865c60
jeanmi <dellxps>
2022-04-27 11:32:45 -0400

    add code with live

--- .ipynb_checkpoints/compute_rmse-checkpoint.py
+++ .ipynb_checkpoints/compute_rmse-checkpoint.py
@@ -2,7 +2,9 @@ import sys
 import json
 import pandas as pd
 from sklearn.metrics import mean_squared_error
+from dvclive import Live
 
+live = Live()
 
 file_submission = sys.argv[1]
 input_folder_path = sys.argv[2]
@@ -19,7 +21,7 @@ for usage in ["Private", "Public"]:
     rmse = mean_squared_error(dfp_to_evaluate_select["Interest"].tolist(), dfp_to_evaluate_select["Prediction"].tolist(), squared=False)
     print(usage, rmse)
     metrics[f"rmse_on_{usage.lower()}_solution"] = rmse
-    
+    live.log(f"rmse_on_{usage.lower()}_solution", rmse)
 with open(file_metrics, "w") as fd:
     json.dump(metrics, fd)
     
