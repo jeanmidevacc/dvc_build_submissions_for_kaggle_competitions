f0113afd1d5e65fd9588bf6521fa358736cd10c2
jeanmi <dellxps>
2022-04-27 12:05:38 -0400

    reinit the seed

--- build_submissions.py
+++ build_submissions.py
@@ -11,7 +11,7 @@ dfp_test = pd.read_csv(input_folder_path + "test.csv").sort_values("Id")
 dfp_solution = pd.read_csv(input_folder_path + "solution.csv").sort_values("Id")
 
 # Build a random sbmission
-# random.seed(params["seed"])
+random.seed(params["seed"])
 dfp_submission = dfp_test.copy()
 dfp_submission["Interest"] = dfp_submission["Id"].apply(lambda id_: int(100 * random.random()))
 dfp_submission.to_csv("./data/random_submission.csv", index=None)
