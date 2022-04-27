151af9c71b35f05f84906f875056f921584ae19e
jeanmi <dellxps>
2022-04-27 10:58:29 -0400

    add code

---
+++ .ipynb_checkpoints/build_submissions-checkpoint.py
@@ -0,0 +1,19 @@
+import random
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+import yaml
+
+params = yaml.safe_load(open("params.yaml"))["build_submissions"]
+
+input_folder_path = "../dvc_build_kaggle_competition/data/kaggle/"
+
+dfp_test = pd.read_csv(input_folder_path + "test.csv").sort_values("Id")
+dfp_solution = pd.read_csv(input_folder_path + "solution.csv").sort_values("Id")
+
+# Build a random sbmission
+random.seed(params["seed"])
+dfp_submission = dfp_test.copy()
+dfp_submission["Interest"] = dfp_submission["Id"].apply(lambda id_: int(100 * random.random()))
+dfp_submission.to_csv("./data/random_submission.csv", index=None)
+
+print("DONE")
---
+++ compute_rmse.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+
+file_submission = sys.argv[1]
+input_folder_path = sys.argv[2]
+file_metrics = sys.argv[3]
+
+dfp_submission = pd.read_csv(file_submission).sort_values("Id")
+dfp_submission.rename(columns={"Interest" : "Prediction"}, inplace=True)
+dfp_solution = pd.read_csv(input_folder_path + "solution.csv").sort_values("Id")
+dfp_to_evaluate = pd.merge(dfp_submission, dfp_solution, on=["Id"])
+
+metrics = {}
+for usage in ["Private", "Public"]:
+    dfp_to_evaluate_select = dfp_to_evaluate[dfp_to_evaluate["Usage"] == usage]
+    rmse = mean_squared_error(dfp_to_evaluate_select["Interest"].tolist(), dfp_to_evaluate_select["Prediction"].tolist(), squared=False)
+    print(usage, rmse)
+    metrics[f"rmse_on_{usage.lower()}_solution"] = rmse
+    
+with open(file_metrics, "w") as fd:
+    json.dump(metrics, fd)
+    
+print("DONE")
